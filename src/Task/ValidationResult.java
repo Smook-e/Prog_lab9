ddac1f086c3a81ca0322763475dadc29e375eca8
Smook-e <ahmednaser2382005@gmail.com>
2025-11-27 12:39:57 +0200

    validationResult

--- src/Task/ValidationResult.java
+++ src/Task/ValidationResult.java
@@ -1,4 +1,24 @@
 package Task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidationResult {
+    public boolean isValid;
+    public final List<String> errors;
+
+    public ValidationResult() {
+        this.isValid = true;
+        this.errors = new ArrayList<>();
+    }
+
+    public void addError(String error) {
+        this.errors.add(error);
+        this.isValid = false;
+    }
+
+    public void merge(ValidationResult other) {
+        this.errors.addAll(other.errors);
+        if (!other.isValid) this.isValid = false;
+    }
 }
